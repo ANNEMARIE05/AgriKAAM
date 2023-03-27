e0df330c36b7f7d5b138459171539385def70bd7
ANNEMARIE05 <annemarieagbonou@gmail.com>
2023-03-27 16:24:26 +0000

    ajout des changement js

---
+++ agrijs/connexion.js
@@ -0,0 +1,30 @@
+let admin = {
+    email: 'admin.second@gmail.com',
+    password: 'SND@admin',
+};
+let superAdmin = {
+    email: 'super.Admin@gmail.com',
+    password: 'superAdmin@',
+};
+
+let email = document.querySelector(".email")
+let passe = document.querySelector(".passe")
+let btn = document.querySelector("button")
+
+
+
+
+btn.addEventListener("click" , function(e){
+   let mail = email.value;
+   let motPasse = passe.value;
+   if(admin.email === mail && admin.password === motPasse){
+    window.location.pathname="../agrihtml/accueil.html";
+    localStorage.setItem("admin",JSON.stringify(admin))
+   }else if(superAdmin.email === mail && superAdmin.password === motPasse){
+    window.location.pathname="../agriadmin/accueilAdmin.html";
+    localStorage.setItem("superAdmin",JSON.stringify(superAdmin))
+   }else{
+    alert("Les champs que vous avez remplir sont incorrects . Vous n'êtes pas autoriser a acceder a la plateforme ")
+   }
+
+})
---
+++ agrijs/travailleur.js
@@ -0,0 +1,133 @@
+let travailleurs = [
+    {
+        id: 1,
+        nom: "anne",
+        prenom: "marie",
+        profession:"programmeuse",
+        contact: "234567890",
+        taches: [
+            {
+                id:1,
+                tache:'dormir',
+                somme:'40000',
+                jour: '22',
+                mois: '11',
+                annee: '1092',
+                etat: "termine"
+            }
+        ]
+    }
+]
+
+
+
+function addTravailleur(travailleurs){
+        localStorage.setItem('travailleurs',JSON.stringify(travailleurs))
+}
+
+// addTravailleur(travailleurs)//
+
+
+
+function getTravailleurs(){
+    let travailleurs = localStorage.getItem('travailleurs')
+    if(travailleurs === null){
+        return []
+    }else{
+        return JSON.parse(travailleurs)
+    }
+}
+
+
+function afficherTravailleurs(){
+    const listOfTravailleurs = document.getElementById('all-ravailleurs');
+    const datalocal = JSON.parse(localStorage.getItem("travailleurs"));
+    console.log(localStorage.getItem("travailleurs"))
+    if(localStorage.getItem("travailleurs")){
+        const all = JSON.parse(localStorage.getItem("travailleurs")).filter(cle=>cle.statut==1);
+        if(all.length > 0){
+            all.forEach(element => {
+                listOfTravailleurs.innerHTML += `<tr>
+                <td>${element.id}</td>
+                <td>${element.nom}</td>
+                <td>${element.prenom}</td>
+                <td>${element.profession}</td>
+                <td>${element.contact}</td>
+                <td>
+                    <a href="modifier.html?id=${element.id}"><i class="fa fa-edit"></i>Modifier</a>
+                    <a href="" onclick='deleteTravailleurs(${element.id})' ><i class="fa fa-trash"></i>Supprimer</a>
+                </td>
+            </tr>`;
+            });
+        }
+    }
+}
+afficherTravailleurs()
+let soumettre = document.getElementById("submit").addEventListener('click', savetravailleur);
+function savetravailleur(event){
+    let nom = document.getElementById('nom').value;
+    let prenom = document.getElementById('prenom').value;
+    let profession = document.getElementById('profession').value;
+    let contact = document.getElementById('contact').value;
+    let data = {
+        id:"",
+        nom:nom,
+        prenom:prenom,
+        profession:profession,
+        contact:contact,
+        statut: 1,
+    }
+    let dataTable = [];
+    if(localStorage.getItem("travailleurs")){
+        data.id ="TRAV00"+JSON.parse(localStorage.getItem("travailleurs")).length;
+        let dataLocal =JSON.parse(localStorage.getItem("travailleurs"));
+        dataLocal.push(data);
+        localStorage.setItem("travailleurs", JSON.stringify(dataLocal));
+    }else{
+        dataTable.push(data);
+        data.id = "TRAV001";
+        localStorage.setItem("travailleurs", JSON.stringify(dataTable));
+    }
+}
+
+function deleteTravailleurs(id){
+    let ourtravailleurs = getTravailleurs()
+    let travailleur = ourtravailleurs.filter((t)=> t.id != id)
+    addTravailleur(travailleur)
+    location.pathname='travailleur.html'
+}
+
+function rechercheTracvailleurs(){
+    let affichage = document.getElementById('reponse-serveur')
+    let recherche = document.getElementById("recherche")
+    let ourtravailleurs = getTravailleurs()
+    recherche.addEventListener("input", (e)=>{
+        let motAChercher = e.currentTarget.value
+        let reponse = ourtravailleurs.filter((t)=>t.nom.toLowerCase()===motAChercher.toLowerCase())
+        if(motAChercher!== ""){
+            if(reponse !== []){
+                affichage.classList.add("reponse")
+                for (const travailleur of reponse) {
+                    affichage.innerHTML+=`
+                    <div class="row">
+                        <p>${travailleur.nom} ${travailleur.prenom} </p>
+                    </div>
+                    `
+                }
+            }else{
+                affichage.classList.remove("reponse")
+            }
+        }else{
+            affichage.classList.remove('reponse')
+            affichage.innerHTML = ""
+            motAChercher = ""
+        }
+    })
+}
+rechercheTracvailleurs()
+
+
+
+function nombreTotalTravailleur(e){
+    
+}
